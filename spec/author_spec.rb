1ba7bde72e9414b269cbfb3f4de2f7ad411edd2b
Darby Smith <newman.darby2@gmail.com>
2022-09-14 11:25:07 -0400

    Add write method test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe Author do
   before (:each) do
     @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    @jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-    @villette = charlotte_bronte.write("Villette", "1853")
+    # @villette = @charlotte_bronte.write("Villette", "1853")
   end
 
   describe '#initialize' do
@@ -20,6 +19,16 @@ RSpec.describe Author do
 
     it 'has no books to start' do
       expect(@charlotte_bronte.books).to eq([])
+    end
+  end
+
+  describe '#write' do
+    it 'can add a book to the books array' do
+      jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      expect(@charlotte_bronte.books).to eq([jane_eyre])
+
+      villette = @charlotte_bronte.write("Villette", "1853")
+      expect(@charlotte_bronte.books).to eq([jane_eyre, villette])
     end
   end
 end
