a50824be199f5bf479409b32fffa98cc185cb3e8
Darby Smith <newman.darby2@gmail.com>
2022-09-14 13:01:01 -0400

    Refactor test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -4,8 +4,10 @@ require './lib/author'
 RSpec.describe Author do
   before (:each) do
     @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    # @jane_eyre = Book.new({author_first_name: "Charlotte", author_last_name: "Bronte", title: "Jane Eyre", publication_date: "October 16, 1847"})
+    # @villette = Book.new({author_first_name: "Charlotte", author_last_name: "Bronte", title: "Villette", publication_date: "1853"})
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    # @villette = @charlotte_bronte.write("Villette", "1853")
+
   end
 
   describe '#initialize' do
@@ -23,12 +25,15 @@ RSpec.describe Author do
   end
 
   describe '#write' do
-    it 'can add a book to the books array' do
+    xit 'can add a book to the books array' do
+      # require 'pry'; binding.pry
       jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      # expect(@charlotte_bronte.books.jane_eyre).to be_instance_of Book
       expect(@charlotte_bronte.books).to eq([jane_eyre])
-
+      
       villette = @charlotte_bronte.write("Villette", "1853")
-      expect(@charlotte_bronte.books).to eq([jane_eyre, villette])
+      # expect(@charlotte_bronte.villette).to be_instance_of Book
+      expect(@charlotte_bronte.books).to be([jane_eyre, villette])
     end
   end
 end
