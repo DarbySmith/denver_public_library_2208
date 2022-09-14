b5e1070bc5ff794b4a392c7ea24c102d1da2eb5a
Darby Smith <newman.darby2@gmail.com>
2022-09-14 12:59:44 -0400

    Refactor write test

--- lib/author.rb
+++ lib/author.rb
@@ -7,4 +7,8 @@ class Author
     @name = "#{@first_name} #{@last_name}"
     @books = []
   end
+
+  def write(title, publication_date)
+    @books << Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date})
+  end
 end
